37198b1b98f4575faab09a8b86a3ed5bb825b7a3
martinbicycle <martinbicycle7700@gmail.com>
2021-03-17 18:43:38 +0900

    [Add]変数

--- test.py
+++ test.py
@@ -6,8 +6,20 @@
 # print("python 難しい"
 
 #演算
-print(1+1)
-print(1-1)
-print(2*2)
-print(10/2)
-print(5%3)
+# print(1+1)
+# print(1-1)
+# print(2*2)
+# print(10/2)
+# print(5%3)
+
+#変数
+title = "Hello World!"
+title_length = 5
+title_times =  5.5
+
+print(type(title))
+print(type(title_length))
+print(type(title_times))
+
+title_problem = False
+print(title_problem)
