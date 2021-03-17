dd17e8c8b772de707dd3bbbfcf272b67d27960b8
martinbicycle <martinbicycle7700@gmail.com>
2021-03-17 19:16:13 +0900

    [Add]リスト

--- test.py
+++ test.py
@@ -39,12 +39,17 @@
 #   print('短い')
 
 # 関数
-def title(arg):
-    title_status = arg
+# def title(arg):
+#     title_status = arg
 
-    if (title_status < 10):
-        return 'まだ大丈夫'
-    else:
-        return 'やばい'
+#     if (title_status < 10):
+#         return 'まだ大丈夫'
+#     else:
+#         return 'やばい'
 
-print(title(12))
+# print(title(12))
+
+
+# list
+title_list = ['title_small', 'title_medium', 'title_large']
+print(title_list[0])
