235153ed709d9fd34f4c75e82b9f9ca80e168470
martinbicycle <martinbicycle7700@gmail.com>
2021-03-17 18:57:59 +0900

    [Add]条件分岐と関係演算子

--- test.py
+++ test.py
@@ -13,13 +13,27 @@
 # print(5%3)
 
 #変数
-title = "Hello World!"
-title_length = 5
+title = "20文字"
+title_length = 0
 title_times =  5.5
 
-print(type(title))
-print(type(title_length))
-print(type(title_times))
+# print(type(title))
+# print(type(title_length))
+# print(type(title_times))
 
-title_problem = False
-print(title_problem)
+# title_problem = False
+# print(title_problem)
+
+#条件分岐と関係演算子
+# if else elif
+# ==, !=, <, >, >=, <=
+
+# if title != '10文字':
+#   print('長い')
+
+if title_length > 6:
+  print('長い')
+elif title_length == 0:
+  print('無い')
+else:
+  print('短い')
